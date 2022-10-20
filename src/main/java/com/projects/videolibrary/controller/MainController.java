22d4e1d46828db1d5dbc21801601f5270b42968d
Volodimir Bezridniy <48326107+Kaktooth@users.noreply.github.com>
2022-10-20 13:16:57 +0300

    - implement selecting categories

--- src/main/java/com/projects/videolibrary/controller/MainController.java
+++ src/main/java/com/projects/videolibrary/controller/MainController.java
@@ -11,6 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
@@ -24,20 +25,27 @@ public class MainController {
 
   @GetMapping
   public String getMainPage(Model model) {
+    addCategoryList(model);
+    Map<Category, List<VideoData>> videoDataByCategory =
+        videoService.getAllVideoDataWithCategories();
+    model.addAttribute("videoDataByCategory", videoDataByCategory);
+
+    return "main";
+  }
+
+  @GetMapping("/{selectCategory}")
+  public String getMainPage(@PathVariable String selectCategory, Model model) {
+    addCategoryList(model);
+    Category selectedCategory = categoryService.getCategoryByName(selectCategory);
+    List<VideoData> selectedVideos = videoService.getVideoDataByCategoryId(
+        selectedCategory.getId());
+    model.addAttribute("selectedCategory", selectedCategory);
+    model.addAttribute("selectedVideos", selectedVideos);
+    return "main";
+  }
+
+  private void addCategoryList(Model model) {
     List<Category> categoryList = categoryService.getAllCategories();
     model.addAttribute("categoryList", categoryList);
-    if (model.getAttribute("category") == null) {
-      Map<Category, List<VideoData>> videoDataByCategory =
-          videoService.getAllVideoDataWithCategories();
-      model.addAttribute("videoDataByCategory", videoDataByCategory);
-    } else {
-      Category category = categoryService.getCategoryByName(
-          (String) model.getAttribute("category"));
-      if (category != null) {
-        List<VideoData> videoData = videoService.getVideoDataByCategoryId(category.getId());
-        model.addAttribute("videoData", videoData);
-      }
-    }
-    return "main";
   }
 }
